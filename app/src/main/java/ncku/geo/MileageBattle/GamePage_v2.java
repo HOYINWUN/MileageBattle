4d1324a0bd9fdeffa7f3e3c412e1a4e6846aeeaa
HOYINWUN <78224rtyioo@gmail.com>
2021-01-06 18:07:55 +0800

    Add table

--- app/src/main/java/ncku/geo/MileageBattle/GamePage_v2.java
+++ app/src/main/java/ncku/geo/MileageBattle/GamePage_v2.java
@@ -38,6 +38,9 @@ public class GamePage_v2 extends AppCompatActivity implements /*LocationListener
     Toast tos;
     int cardset;
     int start_with_player;
+    String[][] country = new String[15][7];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,6 +78,22 @@ public class GamePage_v2 extends AppCompatActivity implements /*LocationListener
 //            ((TextView)findViewById(R.id.textView)).setText("先手:"+countryy_pair[cardset][1]+"\n後手:"+countryy_pair[cardset][0]);
 //        }
         play_music(R.raw.african, 0);
+        country[0] = new String[]{"台灣", "f22", "f12", "f24", "f30", "23.7269838389111", "120.817809452627"};
+        country[1] = new String[]{"日本", "f13", "f18", "f35", "f32", "36.0440762670068", "138.67977098775"};
+        country[2] = new String[]{"韓國", "f11", "f10", "f34", "f41", "39.5341946238494", "126.483851575326"};
+        country[3] = new String[]{"俄羅斯", "f28", "f02", "f20", "f38", "51.5864962071506", "116.020982656694"};
+        country[4] = new String[]{"大陸", "f26", "f21", "f37", "f39", "31.6118306658811", "110.663539674805"};
+        country[5] = new String[]{"菲律賓", "f01", "f25", "f42", "f43", "12.0748037723459", "122.77930438463"};
+        country[6] = new String[]{"泰國", "f42", "f14", "f03", "f23", "17.2410516259493", "102.370176114122"};
+        country[7] = new String[]{"孟加拉", "f00", "f36", "f33", "f06", "23.5694106442576", "90.1561184829716"};
+        country[8] = new String[]{"印尼", "f33", "f00", "f42", "f43", "-1.87756262289149", "120.552776614628"};
+        country[9] = new String[]{"馬來西亞", "f15", "f42", "f16", "f08", "4.47354331900812", "101.706514897057"};
+        country[10] = new String[]{"越南", "f42", "f14", "f37", "f43", "13.1558692515745", "107.988602136247"};
+        country[11] = new String[]{"蒙古", "f17", "f29", "f27", "f03", "46.222093115052", "104.950649266283"};
+        country[12] = new String[]{"緬甸", "f03", "f07", "f42", "f40", "22.5691826871558", "96.0895898361343"};
+        country[13] = new String[]{"新加坡", "f15", "f19", "f05", "f42", "1.31484423606705", "103.873186930365"};
+        country[14] = new String[]{"香港", "f44", "f09", "f37", "f31", "22.3927056941769", "114.050231584071"};
+
 
     }
 /*
